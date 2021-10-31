24720886a3cc3c1b7a159f8eed05374b9f06f520
sam-omar322 <sam.omar564@gmail.com>
2021-10-31 18:33:26 +0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var optimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+ 
+  entry:  {
+    app:'./src/index.js'
+  },
+  
+  output: {
+    path: path.join(__dirname, "/dist"),
+    publicPath: '',
+    filename: "main.js"
+  },
+
+  mode: "development",
+
+  devServer: {
+    contentBase: path.join(__dirname, "/dist"),
+    port: 8080,
+    open: true,
+    writeToDisk: true
+  },
+
+  module: {
+    rules: [
+
+      {
+        test: /\.(png|svg|jpe?g|gif)$/,
+        use: [
+          {
+            loader: "file-loader", 
+            options: {
+              name: '[name].[ext]',
+              outputPath: "images",
+            }
+          }
+        ]
+      },
+      
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader,
+              'css-loader']
+      },
+
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {minimize: true}
+          }
+        ]
+      }
+    ]
+  },
+ 
+  plugins: [
+
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+
+    new MiniCssExtractPlugin({
+        filename: "/css/style.css",
+    }),
+    new optimizeCssAssetsWebpackPlugin({})
+  ],
+
+};
