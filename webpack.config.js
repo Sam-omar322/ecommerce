b922224b94e5adb810b11c18d2e228a4a1edb5d3
sam-omar322 <sam.omar564@gmail.com>
2021-11-07 13:06:10 +0300

    added new page src/contact.html and modified in src/css/style.css

--- webpack.config.js
+++ webpack.config.js
@@ -113,6 +113,11 @@ module.exports = {
       template: "./src/search.html",
     }),
 
+    new HtmlWebpackPlugin({
+      filename: "contact.html",
+      template: "./src/contact.html",
+    }),
+
     new MiniCssExtractPlugin({
         filename: "/css/style.css",
     }),
