9b96b88d5af590d3e0932359e75be1023e35d9c8
sam-omar322 <sam.omar564@gmail.com>
2021-11-10 18:14:59 +0300

    added bootstrap-sass and new file  src/scss/custom.scss and changed all custom-btn-primary2 into btn-primary and src/index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import "@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css";
+// import "@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css";
+// import "@laylazi/bootstrap-rtl-scss/scss/bootstrap-rtl.scss";
 import "./scss/style.scss";
 import "./css/style.css";
-import "./css/test.css";
 import "bootstrap";
 import "jquery/dist/jquery.min";
 import "popper.js/dist/popper.min";
